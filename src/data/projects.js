a3094bd0faf6a07a33c329160a6e5dff1ca0282c
oscarporras02 <oscarrussi@outlook.com>
2020-12-11 20:52:20 -0500

    add valid data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,9 @@
 import Project from '../classes/project';
 
 const projects = {
-    1: Project(1, 'todo project', 'this is a todo web project'),
-    2: Project(2, 'restaurant project', 'this is a restaurant web project'),
-    3:  Project(3, 'library project', 'this is a library web project')
+    1: Project(1, 'todo project', 'this is the todo web project'),
+    2: Project(2, 'restaurant project', 'this is the restaurant web project'),
+    3:  Project(3, 'library project', 'this is the library web project')
 };
 
   export default projects;
--- src/data/todos.js
+++ src/data/todos.js
@@ -1,9 +1,12 @@
 import Todo from '../classes/todo';
 
 const todos = {
-    1: Todo(1, 'todo project', 'this is a todo web project', 1, 2, '12/10/2020'),
-    2: Todo(2, 'restaurant project', 'this is a restaurant web project', 1, 2, '12/10/2020'),
-    3:  Todo(3, 'library project', 'this is a library web project',1 , 2, '12/10/2020')
+    1: Todo(1, 'create form for projects', 'this is the to-do for creating form for projects', 1, 2, '08/09/2021'),
+    2: Todo(2, 'create a function to delete projects', 'this is the to-do for creating a function to delete projects', 1, 2, '09/10/2019'),
+    3:  Todo(3, 'create a class for projects', 'this is the to-do for creating a class for projects',1 , 1, '02/08/2020'),
+    4: Todo(4, 'add a tab for the menus', 'this is the to-do for adding a tab for the menus', 2, 3, '10/10/2021'),
+    5: Todo(5, 'add a model for the menus', 'this is the to-do for adding a model for the menus', 2, 2, '11/10/2020'),
+    6:  Todo(6, 'create a class for the books', 'this is the to-do for creating a class for the books', 3 , 1, '01/10/2021')
 };
 
   export default todos;
