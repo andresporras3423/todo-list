d6c47ab1067a5ac117880b91cb96b39b69bafe72
oscarporras02 <oscarrussi@outlook.com>
2020-12-23 13:13:31 -0500

    add tests for Todo class

--- src/classes/todo.js
+++ src/classes/todo.js
@@ -1,12 +1,12 @@
-const Todo = (id, name, description, idProject, priority, duedate) => (
-  {
-    id,
-    name,
-    description,
-    idProject,
-    priority,
-    duedate,
+class Todo {
+  constructor(id, name, description, idProject, priority, duedate){
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.idProject = idProject;
+      this.priority = priority;
+      this.duedate = duedate;
   }
-);
+}
 
 export default Todo;
